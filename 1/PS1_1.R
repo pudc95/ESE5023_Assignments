17b9bc66c99479bfa652393cc9c553e20ac89f53
pudc95 <42628110+pudc95@users.noreply.github.com>
2020-10-10 18:03:42 +0800

    Update PS1_1.R

--- 1/PS1_1.R
+++ 1/PS1_1.R
@@ -1,1 +1,36 @@
-aa
+#1
+Print_values <- function(a, b, c){
+  if(a > b){
+    if(b > c){
+      sequence  <- c(a, b ,c)
+    }
+    else{
+      if(a > c){
+        sequence  <- c(a, c, b)
+      }
+      else{
+        sequence  <- c(c, a, b)
+      }
+    }
+  }
+  else{
+    if(b > c){
+      if(a > c){
+        sequence  <- c(a, c, b)
+      }
+      else{
+        sequence  <- c(c, a, b)
+      }
+    }
+    else
+      sequence  <- c(c, b, a)
+  }
+  print(sequence)
+}
+
+a = runif(1)
+b = runif(1)
+c = runif(1)
+
+Print_values(a, b, c)
+  
